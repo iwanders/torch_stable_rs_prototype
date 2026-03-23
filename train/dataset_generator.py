7cdf58af66ee9b42bebfd604b13e16a0547ee129
Ivor Wanders <ivor@iwanders.net>
2026-03-23 19:48:52 -0400

    Loading both foreground and background.

--- train/dataset_generator.py
+++ train/dataset_generator.py
@@ -3,6 +3,7 @@
 
 from pathlib import Path
 
+import numpy as np
 import torchvision
 from PIL import Image
 from torchvision.transforms import ToTensor
@@ -12,17 +13,22 @@ class DatasetGenerator:
     def __init__(self, background_dir, foreground_dir):
         self._background_dir = Path(background_dir)
         self._foreground_dir = Path(foreground_dir)
-        self._background_top_left = (100, 50)
-        self._background_size = (1700, 800)
+        self._background_crop_top_left = (100, 50)
+        self._background_crop_size = (1700, 800)
 
         self._background_images = []
+        self._foreground_images = []
 
-    def load_background_image(self, d):
+    def load_image(self, d):
         image = Image.open(d)
         image = ToTensor()(image)
-        left, top = self._background_top_left
-        bottom = top + self._background_size[1]
-        right = left + self._background_size[0]
+        return image
+
+    def load_background_image(self, d):
+        image = self.load_image(d)
+        left, top = self._background_crop_top_left
+        bottom = top + self._background_crop_size[1]
+        right = left + self._background_crop_size[0]
         image = image[
             :,
             top:bottom,
@@ -34,16 +40,21 @@ class DatasetGenerator:
         for f in self._background_dir.iterdir():
             background_image = self.load_background_image(f)
             self._background_images.append(background_image)
+        for f in self._foreground_dir.iterdir():
+            self._foreground_images.append(self.load_image(f))
 
     def debug_dump(self):
         output = Path("/tmp/debug_dump")
         output.mkdir(exist_ok=True)
         for i, img in enumerate(self._background_images):
             torchvision.utils.save_image(img, output / f"background_{i}.png")
+        for i, img in enumerate(self._foreground_images):
+            torchvision.utils.save_image(img, output / f"foreground_{i}.png")
 
 
 if __name__ == "__main__":
     background_dir = "../../datasets/background/cave/"
-    d = DatasetGenerator(background_dir, foreground_dir=".")
+    foreground_dir = "../../datasets/foreground/cave/"
+    d = DatasetGenerator(background_dir, foreground_dir=foreground_dir)
     d.load_images()
     d.debug_dump()
