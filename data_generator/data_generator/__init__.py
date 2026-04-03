46e6d7ea075c9e1ef6714012c192c303ffbee48f
Ivor Wanders <ivor@iwanders.net>
2026-04-03 11:12:10 -0400

    Remove unrelated code.

--- data_generator/data_generator/__init__.py
+++
@@ -1,4 +0,0 @@
-from .data_generator import sum_as_string
-
-hello = 3
-z = sum_as_string(3, 3)
--- data_generator/data_generator/test.py
+++
@@ -1,9 +0,0 @@
-from .data_generator import make_tensor, sum_as_string
-
-hello = 3
-z = sum_as_string(3, 3)
-print(z)
-
-a = make_tensor()
-print(a)
-print(dir(a))
