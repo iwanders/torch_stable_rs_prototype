23d2dfa889fc534ca06d41b1ccd5844c7fee74c2
Ivor Wanders <ivor@iwanders.net>
2026-04-30 19:33:29 -0400

    Add fill to suppressions, it causes an uninitialised value.

--- torch_stable/valgrind/main.cpp
+++ torch_stable/valgrind/main.cpp
@@ -23,8 +23,29 @@ void run_torch_stable_to() {
 }
 
 
+void run_torch_stable_fill(){
+  std::cout << "suppression@dispatch:aten::fill_Tensor" << std::endl;
+
+  const auto self = torch::stable::empty(torch::headeronly::HeaderOnlyArrayRef<int64_t>{5,5}, {}, {}, {}, {}, {});
+  // let mut t = Tensor::zeros(&[5, 5], &Default::default())?;
+
+  AtenTensorHandle ret0;
+  STABLE_TORCH_ERROR_CODE_CHECK( aoti_torch_scalar_to_tensor_float32(3.3, &ret0));
+  const auto v =  torch::stable::Tensor(ret0);
+
+
+  const auto num_args = 2;
+  std::array<StableIValue, num_args> stack{
+      torch::stable::detail::from(self),
+      torch::stable::detail::from(v)};
+  STABLE_TORCH_ERROR_CODE_CHECK(torch_call_dispatcher(
+      "aten::fill", "Tensor", stack.data(), TORCH_ABI_VERSION));
+  (void)torch::stable::detail::to<torch::stable::Tensor>(stack[0]);
+}
+
 int main(int argc, char* argv[]){
   run_torch_stable_empty();
   run_torch_stable_to();
+  run_torch_stable_fill();
   return 0;
 }
